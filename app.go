c7c3c917f0eec81682633fc5caabdc02c2a2449c
Jonthan King <jonno.king@gmail.com>
2019-10-20 14:05:49 +0100

    team

--- app.go
+++ app.go
@@ -10,6 +10,7 @@ import (
 	"github.com/joho/godotenv"
 	fpl "github.com/jonnoking/vidukavindaloo-fpl"
 	"github.com/jonnoking/vidukavindaloo-fpl/config"
+	"github.com/jonnoking/vidukavindaloo-fpl/models"
 )
 
 var FPL *fpl.FPL
@@ -37,6 +38,28 @@ func main() {
 			},
 			Action: playerSearch,
 		},
+		cli.Command{
+			Name:        "team",
+			Aliases:     []string{"tm"},
+			Category:    "Teams",
+			Usage:       "get a team",
+			UsageText:   "",
+			Description: "",
+			ArgsUsage:   "[]",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "short-name",
+					Value: "",
+					Usage: "The team's short name",
+				},
+				cli.StringFlag{
+					Name:  "name",
+					Value: "",
+					Usage: "The team's name",
+				},
+			},
+			Action: teamShortSearch,
+		},
 	}
 
 	err := app.Run(os.Args)
@@ -57,10 +80,66 @@ func playerSearch(c *cli.Context) error {
 		log.Fatalf("Could not find the player %s\n", player)
 	}
 
+	fmt.Println()
+	printPlayer(&p)
+	fmt.Println()
+
+	return nil
+}
+
+func teamShortSearch(c *cli.Context) error {
+
+	team := c.String("short-name")
+	if team == "" {
+		log.Fatal("No short name supplied")
+	}
+
+	t, err := FPL.Bootstrap.Teams.GetTeamByShortName(team)
+	if err != nil {
+		log.Fatalf("%s\n", err.Error())
+	}
+
+	fmt.Println()
+	fmt.Printf("%s (%s)\n", t.Name, t.ShortName)
+	fmt.Printf("* Strength: %d\n", t.Strength)
+	fmt.Println()
+
+	g, d, m, f := t.GetSquad(FPL.Bootstrap.Players)
+
+	printPosition(g, "Goalkeepers", false)
+	fmt.Println()
+	printPosition(d, "Defenders", false)
+	fmt.Println()
+	printPosition(m, "Midfielders", false)
+	fmt.Println()
+	printPosition(f, "Forwards", false)
+	fmt.Println()
+
+	return nil
+}
+
+func printPosition(pos map[int]models.Player, position string, details bool) {
+	fmt.Printf("### %s ###\n", position)
+	for _, ff := range pos {
+		if details {
+			printPlayerDetails(&ff)
+		} else {
+			printPlayer(&ff)
+		}
+	}
+
+}
+
+func printPlayer(p *models.Player) {
+	nc := float32(p.NowCost)
+	ncd := nc / 10
+	fmt.Printf("%s [£%.1fm] (%s)\n", p.GetFullName(), ncd, p.GetTeam(FPL.Bootstrap.Teams).Name)
+}
+
+func printPlayerDetails(p *models.Player) {
 	nc := float32(p.NowCost)
 	ncd := nc / 10
 
-	fmt.Println()
 	fmt.Printf("%s (%s)\n", p.GetFullName(), p.GetTeam(FPL.Bootstrap.Teams).Name)
 	fmt.Printf("* Position: %s\n", p.GetPlayerType(FPL.Bootstrap.PlayerTypes).SingularName)
 	fmt.Printf("* Total Points: %d\n", p.TotalPoints)
@@ -68,9 +147,6 @@ func playerSearch(c *cli.Context) error {
 	if p.News != "" {
 		fmt.Printf("* News: %s\n", p.News)
 	}
-	fmt.Println()
-
-	return nil
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module vidukavindaloo-cli
 go 1.13
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/joho/godotenv v1.3.0
-	github.com/jonnoking/vidukavindaloo-fpl v0.0.0-20191018113704-dadaa8314555
+	github.com/jonnoking/vidukavindaloo-fpl v0.0.0-20191020121512-e98290507142
 	github.com/urfave/cli v1.22.1
 )
